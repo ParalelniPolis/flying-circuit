35407e391dbafe78eb7c27707b0a40de42b10bf5
Michal Takac <takacmichal92@gmail.com>
2019-04-27 13:20:08 +0200

    fix assetPrefix #2

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@ const withCSS = require('@zeit/next-css')
 
 module.exports = withCSS({
   crossOrigin: 'anonymous',
-  assetPrefix: process.env.NODE_ENV === "production" ? "" : "/out"
+  assetPrefix: process.env.NODE_ENV === "production" ? "/" : "/out"
 })
