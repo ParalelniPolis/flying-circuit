874a25ec8fa5d2c899de346fbae00da77daea643
Michal Takac <takacmichal92@gmail.com>
2019-04-28 17:51:35 +0200

    add GOOGLE_SCRIPT_URL

--- config/index.js
+++ config/index.js
@@ -3,5 +3,6 @@ export default {
     title: "Hackatoshi's Flying Circuit",
     description: "Hackathon",
     url: "https://example.com"
-  }
+  },
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzzzic64flcbwKdrxVbS3kIla7TEsPEP6iTAVTn8-WeUK5miuIa/exec";
 }
