1bb8877559e56e688371edbc3500f71a7df36d6f
Michal Takac <takacmichal92@gmail.com>
2019-04-28 18:19:16 +0200

    fix: remove semicolon

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ export default {
   seo: {
     title: "Hackatoshi's Flying Circuit",
     description: "Hackathon",
-    url: "https://example.com"
+    url: "https://example.com",
   },
-  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzzzic64flcbwKdrxVbS3kIla7TEsPEP6iTAVTn8-WeUK5miuIa/exec";
-}
+  GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzzzic64flcbwKdrxVbS3kIla7TEsPEP6iTAVTn8-WeUK5miuIa/exec",
+};
